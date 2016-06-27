d0306fd69a34c4637628d3e54f6f2c163295320a
Lotte Steenbrink <lotte@zombietetris.de>
2016-06-27 14:58:49 +0200

    user_connector.py create regular voip connections (WIP)
    connections on access and gateway levels done, backbone TBD

--- ex7/user_connector.py
+++ ex7/user_connector.py
@@ -1,10 +1,13 @@
 #!/usr/bin/python3
 
 import argparse
+import random
 
+str_connection = "**.user[{}].udpApp[0].destAddresses = \"user[{}]\"\n"
 
 parser = argparse.ArgumentParser(description='Generates a user connection file to be included in an omnet.ini\nTakes care of connecting users over the farthest distance, i.e. the internet gateway (therefore a different backbone), to accumulate worst case propagation delays.')
 
+parser.add_argument('-voip', action='store_true', help='configure connections for Config VoiceTest')
 parser.add_argument('-b', dest='num_bb', type=int, default=2, help='Backbone routers (default: 2)')
 parser.add_argument('-a', dest='num_access_per_bb', type=int, default=1, help='Access routers per backbone router (default: 1)')
 parser.add_argument('-u', dest='num_user_per_access', type=int, default=2, help='Users per access router (default: 2)')
@@ -14,8 +17,10 @@ args = parser.parse_args()
 
 print("Arguments:", args.num_bb, args.num_access_per_bb, args.num_user_per_access)
 
+num_access_total = args.num_access_per_bb * args.num_bb
 users_per_backbone = args.num_access_per_bb * args.num_user_per_access
 total_users = args.num_bb * users_per_backbone
+num_user_per_access = args.num_user_per_access
 
 if args.num_bb <= 0 or args.num_access_per_bb <= 0:
 	print("Invalid arguments: Less or equal to zero.\nExiting...")
@@ -31,9 +36,15 @@ if total_users == 1:
 
 print('Total users:', total_users, "\n")
 
-def create_connection_file():
-	str_connection = "**.user[{}].udpApp[0].destAddresses = \"user[{}]\"\n"
+if args.voip:
+	# we're creating a voip config, not the worst case config
+	str_fname = "user_connections_voicetest.ini"
+else:
+	# worst case config file (this is the default)
+	str_fname = "user_connections.ini"
 
+
+def create_connection_file():
 	str_con_file = "# This file includes worst case user connections for omnet++/exercise7\n"
 
 	for u in range(0, total_users): #i.e.: total_users-1
@@ -43,12 +54,79 @@ def create_connection_file():
 
 	return str_con_file
 
+def create_connection_file_voip():
+	str_con_file = "# This file includes regular voip user connections for omnet++/exercise7\n"
+	# just make sure we always get the same connections
+	random.seed(23)
+
+	# record which users are already in a call so we don't connect them twice
+	connected_users = []
+
+	# create connections that use the gateway
+
+	str_con_file += "# connections that use the gateway\n"
+	for c in range(0, 6):
+
+		# search for 2 random users to connect
+		for i in range(0,2):
+			user_found = False
+			while (not user_found):
+				user = random.randint(0, total_users-1)
+				if (user not in connected_users):
+					connected_users.append(user)
+					user_found = True
+
+		# connect them
+		str_con_file += str_connection.format(connected_users[-1], connected_users[-2])
+		str_con_file += str_connection.format(connected_users[-2], connected_users[-1])
+		str_con_file += "\n"
+
+	# create connections that use a backbone router
+
+	# create connections that use only an access router
+	str_con_file += "# connections that use only an access router\n"
+
+	for c in range (0, 16):
+		# pick a router
+		router_no = random.randint(0, num_access_total-1)
+
+		# connect 2 users at router_no
+		for i in range (0,2):
+			user_found = False
+
+			while (not user_found):
+				# pick a user (number is local to the access points)
+				user_no = random.randint(0, num_user_per_access-1)
+				# locate user's position in whole tree
+				user_pos = (router_no * num_user_per_access) + user_no
+				if (user_pos not in connected_users):
+					connected_users.append(user_pos)
+					user_found = True
+
+				print("user_no", user_no)
+				print("router_no", router_no)
+				print("user_pos", user_pos)
+
+		# connect them
+		str_con_file += str_connection.format(connected_users[-1], connected_users[-2])
+		str_con_file += str_connection.format(connected_users[-2], connected_users[-1])
+		str_con_file += "\n"
+
+		# TODO: if there are no connections left for this router, move on
+
+
+	print("The connection file looks like this:\n\n" + str_con_file)
+
+	return str_con_file
 
 def main():
-	
-	str_con_file = create_connection_file()
-	str_fname = "user_connections.ini"
-	
+	if (str_fname == "user_connections_voicetest.ini"):
+		str_con_file = create_connection_file_voip()
+	else:
+		str_con_file = create_connection_file()
+
+	# write connections crested with create_connection_file()
+	# or create_connection_file_voip() to file
 	f_omnet_include = open(str_fname, "w")
 	f_omnet_include.write(str_con_file)
 	f_omnet_include.close()
