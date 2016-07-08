8979faad216e740fb2e3f87365179e81ecfed5a7
Lotte Steenbrink <lotte@zombietetris.de>
2016-07-08 11:19:05 +0200

    increase simtime, switch to VoIP-App

---
+++ ex_8/VoIPApp.cc
@@ -0,0 +1,59 @@
+#include <omnetpp.h>
+#include "VoIPApp.h"
+
+Define_Module(VoIPApp);
+
+
+
+simsignal_t VoIPApp::packetDelaySignal = registerSignal("VoIPPacketDelay");
+
+
+void VoIPApp::initialize(int stage)
+{
+    UDPBasicApp::initialize(stage);
+
+    if (stage == 0)
+    {
+        numDiscarded = 0;
+        WATCH(numDiscarded);
+    }
+}
+
+void VoIPApp::processPacket(cPacket *pk)
+{
+    emit(rcvdPkSignal, pk);
+    EV << "Received packet: " << UDPSocket::getReceivedPacketInfo(pk) << endl;
+	
+
+	simtime_t endToEndDelay = pk->getArrivalTime() - pk->getCreationTime();
+	emit(packetDelaySignal, endToEndDelay);
+	EV << "SendTime: " << pk->getCreationTime() << endl;
+	EV << "ArrivalTime: " << pk->getArrivalTime() << endl;
+	EV << "CurrentTime: " << simTime() << endl;
+	EV << "EndToEndDelay: " << endToEndDelay << endl;
+	if(endToEndDelay > par("maxDelay")) {
+		EV << "Packet discarded" << endl;
+		numDiscarded++;
+	}
+	numReceived++;
+	delete pk;
+}
+
+void VoIPApp::sendPacket()
+{
+    char msgName[32];
+    sprintf(msgName, "UDPBasicAppData-%d", numSent);
+    cPacket *payload = new cPacket(msgName);
+    payload->setByteLength(par("messageLength").longValue());
+    IPvXAddress destAddr = chooseDestAddr();
+
+    emit(sentPkSignal, payload);
+    socket.sendTo(payload, destAddr, destPort);
+    numSent++;
+}
+
+void VoIPApp::finish() {
+    UDPBasicApp::finish();
+    recordScalar("lateRatio", (float)numDiscarded/numReceived);
+    recordScalar("discarded packets", numDiscarded);
+}
---
+++ ex_8/VoIPApp.h
@@ -0,0 +1,18 @@
+#include "UDPBasicApp.h"
+
+#include "IPvXAddressResolver.h"
+#include "ILifecycle.h"
+#include "LifecycleOperation.h"
+
+class VoIPApp : public UDPBasicApp
+{
+private:
+    int numDiscarded;
+    static simsignal_t packetDelaySignal;
+public:
+    void initialize(int stage);
+	void processPacket(cPacket *pk) override;
+	void sendPacket() override;
+	void finish() override;
+};
+
