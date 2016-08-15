9dc3a526b592704fd9a48d8e2eca8df6b949bb15
Alexander Hillmer <dev.alexander.hillmer@gmail.com>
2016-08-15 18:52:49 +0200

    Added windows remote simulation scripts. pscp = scp, plink = ssh

---
+++ remote_scripts_win/runRemote.bat
@@ -0,0 +1,4 @@
+start cmd /k plink -ssh -l siah1097 -pw pw l00p31.rz.tu-harburg.de -m "package1.txt"
+start cmd /k plink -ssh -l siah1097 -pw pw l01p31.rz.tu-harburg.de -m "package2.txt"
+start cmd /k plink -ssh -l siah1097 -pw pw l02p31.rz.tu-harburg.de -m "package3.txt"
+start cmd /k plink -ssh -l siah1097 -pw pw l03p31.rz.tu-harburg.de -m "package4.txt"
---
+++ remote_scripts_win/uploadExamTask.bat
@@ -0,0 +1,2 @@
+7z.exe a -ttar examTask.tar examTask
+pscp examTask.tar siah1097@ssh.rz.tu-harburg.de:/home/siah1097/linux/workspace/examTask.tar
