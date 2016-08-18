b2f291aca524c7fb63d07c6949a543ff00f29e89
Lotte Steenbrink <lotte@zombietetris.de>
2016-08-19 00:39:05 +0200

    poissonChi2Test.m: remove commented out code chunks

--- matlab/poissonChi2Test.m
+++ matlab/poissonChi2Test.m
@@ -22,7 +22,7 @@ function [ fits, ratio ] = poissonChi2Test( samples, binSize,alpha )
        else
            n = n +1;
        end
-       
+
        maxN = length(sampleHist);
     end
 
@@ -36,54 +36,13 @@ function [ fits, ratio ] = poissonChi2Test( samples, binSize,alpha )
            sampleWidths(n) = [];
        end
        n = n -1;
-       
+
     end
 
-
-    %fprintf('Adjusted Histogram:\n');
     sampleHist;
     sampleWidths;
     %bar(sampleHist, sampleWidths);
     %hold on;
-
-    %generate expected values
-    %{
-        lambdasample = 0;
-        for n=1:length(samples)
-           lambdasample = lambdasample + samples(n);
-        end
-
-        lambdasample = lambdasample/length(samples);
-
-
-        P = 0
-        for n = 1:length(sampleWidths)
-            if(n == 1)
-                P(n) = poisscdf(sampleWidths(n),lambdasample);
-            else
-                P(n) = poisscdf(sampleWidths(n),lambdasample) - poisscdf(sampleWidths(n-1),lambdasample);
-            end
-        end
-
-        NP = P.*length(samples)
-        bar(P);
-
-        chi2query = 0;
-        for n = 1:length(P)
-            sub = sampleHist(n)-NP(n);
-            chi2query = (sub*sub) / NP(n);
-        end
-
-        chi2level = chi2inv(1-alpha, length(sampleHist)-dof-1);
-        chi2ratio = chi2query/chi2level;
-        fprintf('Chi2Sum: %.02f\n',chi2query);
-        fprintf('Chi2Level: %.02f\n', chi2level);
-        fprintf('Chi2Ratio: %.05f\n', chi2ratio);
-    %}
-    
-    
-    
-    
 
     pExponential = fitdist(samples, 'Exponential');
     pPoisson = fitdist(samples, 'Poisson');
@@ -91,24 +50,16 @@ function [ fits, ratio ] = poissonChi2Test( samples, binSize,alpha )
     y = exppdf(x, pExponential.mu);
     %y = poisspdf(x, 1/pPoisson.lambda);
     plot(x,y,'+')
-    
-    
-    %sampleWidths(1) = []
-    %sampleWidths(length(sampleWidths)) = []
-    %for n = 1:length(sampleWidths)-1
-    %   sampleWidths(n) = sampleWidths(n+1);
-    %end
-    %sampleWidths(1) = [];
+
     [hExp, pExp, statsExp] = chi2gof(samples, 'Edges', sampleWidths, 'CDF', pExponential, 'Alpha', alpha)
     [hPois, pPois, statsPois] = chi2gof(samples, 'Edges', sampleWidths, 'CDF', pPoisson, 'Alpha', alpha)
-    %[hExp, pExp, statsExp] = chi2gof(samples, 'Edges', sampleWidths, 'CDF', pExponential, 'Alpha', alpha);
-    
-    hPois
+
+    %hPois
     %hExp
-    
-    pPois
+
+    %pPois
     %pExp
-    
+
     %fits = chi2query <= chi2level;
     %ratio = chi2ratio;
 
