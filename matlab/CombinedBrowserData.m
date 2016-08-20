85a746308af136455eb3cda966c47b7fa6615841
Alexander Hillmer <dev.alexander.hillmer@gmail.com>
2016-08-20 16:49:47 +0200

    Splitted Browser Evaluation from the rest.

---
+++ matlab/CombinedBrowserData.m
@@ -0,0 +1,43 @@
+simtime = 1000;
+repititions = 15;
+alpha = 0.05;
+%Prepend folder for result set
+imageDirectory = 'images/finalnocctv/';
+%The amount of clients
+x = [1,5,10,15,20,30,40,50,60];
+
+
+% fileBase contains the path to result data file up to the run number
+fileBase = '../results/final1-nocctv-160820/ExamTaskNetwork_no_CCTV-'
+% fileStartNr denotes the first run number
+fileStartNr = 0;
+% fileEndNr denotes the last run number
+fileEndNr = 134;
+% the search array contains the modulename and parameter name to look at
+% the data is extracted for each row
+searchArray = {'BrowsingLaptop','tcpApp[0]', 'rcvdPk:sum(packetBytes)';
+    'BrowsingLaptop','tcpApp[0]','sentPk:sum(packetBytes)'}
+[ result ] = extractDataMulti( fileBase, fileStartNr, fileEndNr, searchArray);
+
+throughputRcvdBrowser = result3(:,1) ./ simtime;
+throughputSendBrowser = result3(:,2) ./ simtime;
+
+
+
+
+modResults = [throughputSendBrowser throughputRcvdBrowser];
+
+% calculate confidence intervals
+[mean, e] = confIntervals( modResults, repititions, alpha);
+
+
+resultArray = { 'Browser', 'send throughput all','bytes/s';
+                'Browser', 'rcvd throughput all','bytes/s'}
+            
+%plot each row of mean and confidence intervals
+% A row corresponds to the row in the search array
+for i=1:size(mean, 1)
+    title = strcat(resultArray{i,1},' - ', resultArray{i,2});
+    yunit = resultArray{i,3};
+    plotConf(imageDirectory, title, x, mean(i,:), e(i,:), '# of clients', yunit);
+end
