c376a7b6b9c9f27a08491fe4326cd5954409d502
Alexander Hillmer <dev.alexander.hillmer@gmail.com>
2016-08-22 13:46:36 +0200

    Added extractTest.m for RAP throughput

---
+++ matlab/extractTest.m
@@ -0,0 +1,52 @@
+% This script calculates the confidence intervals for the lateRatios of the
+% voip/video streams of the profesors and conference laptops and plots them
+
+simtime = 1000;
+repititions = 15;
+alpha = 0.05;
+%Prepend folder for result set
+imageDirectory = 'images/finalnocctv/';
+%The amount of clients
+x = [1,5,10,15,20,30,40,50,60];
+%x = [1,5,10];
+
+
+% fileBase contains the path to result data file up to the run number
+fileBase = '../results/final2-nocctv-160821/ExamTaskNetwork_no_CCTV-'
+% fileStartNr denotes the first run number
+fileStartNr = 0;
+% fileEndNr denotes the last run number
+fileEndNr = 134;
+% the search array contains the modulename and parameter name to look at
+% the data is extracted for each row
+searchArray = {'RemoteAccessPoint.wlan[0].mac','rcvdPkFromLL:sum(packetBytes)';
+    'RemoteAccessPoint.wlan[0].mac','sentDownPk:sum(packetBytes)'}
+[ result ] = extractDataSca( fileBase, fileStartNr, fileEndNr, searchArray );
+throughputFromClients = (result(:,1) .* 8) ./ (simtime*1024*1024);
+throughputToClients = (result(:,2) .* 8) ./ (simtime*1024*1024);
+throughput = throughputFromClients+throughputToClients;
+
+
+
+
+%queueAvgWaitRemote = result2(:,1);
+%queueAvgWaitMain = result2(:,2);
+modResults = [throughputFromClients'
+              throughputToClients'
+              throughput']';
+
+% calculate confidence intervals
+[mean, e] = confIntervals( modResults, repititions, alpha);
+
+
+resultArray = {'RAP','throughput from clients','Mbps';
+    'RAP','throughput to clients','Mbps'
+    'RAP','throughput combined','Mbps'};
+            
+%plot each row of mean and confidence intervals
+% A row corresponds to the row in the search array
+for i=1:size(mean, 1)
+    title = strcat(resultArray{i,1},' - ', resultArray{i,2});
+    yunit = resultArray{i,3};
+    plotConf(imageDirectory, title, x, mean(i,:), e(i,:), '# of clients', yunit);
+end
